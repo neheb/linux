e13f2c7612e60ccaf7b18f8b38f2dd22aecb9699
Rosen Penev <rosenp@gmail.com>
2024-09-18 13:02:50 -0700

    nfc: pn533: fix wrong memcpy size
    This is overwriting the next member which gets memcpy called on it
    anyway.
    
    Fixes:
    
    call to ‘__read_overflow2_field’ declared with attribute warning:
    detected read beyond size of field (2nd parameter); maybe use
    struct_group()?
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- drivers/nfc/pn533/pn533.c
+++ drivers/nfc/pn533/pn533.c
@@ -778,7 +778,7 @@ static int pn533_target_found_felica(struct nfc_target *nfc_tgt, u8 *tgt_data,
 	else
 		nfc_tgt->supported_protocols = NFC_PROTO_FELICA_MASK;
 
-	memcpy(nfc_tgt->sensf_res, &tgt_felica->opcode, 9);
+	memcpy(nfc_tgt->sensf_res, &tgt_felica->opcode, 1);
 	nfc_tgt->sensf_res_len = 9;
 
 	memcpy(nfc_tgt->nfcid2, tgt_felica->nfcid2, NFC_NFCID2_MAXSIZE);
