92888488614a8f3a60d75b7372e19251d2ce2f6e
Rosen Penev <rosenp@gmail.com>
2024-10-22 16:54:54 -0700

    net: memcpy to ethtool_puts
    The latter is recommended to avoid manual pointer addition.
    
    Transformed with the follow coccinelle script:
    
    virtual org
    virtual report
    virtual context
    virtual patch
    
    @depends on patch@
    expression E1, E2;
    identifier I;
    position p;
    @@
    
    - memcpy@p(E1 + I * ETH_GSTRING_LEN, E2, ETH_GSTRING_LEN);
    + ethtool_puts(&E1, E2);
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- drivers/net/xen-netback/interface.c
+++ drivers/net/xen-netback/interface.c
@@ -453,8 +453,7 @@ static void xenvif_get_strings(struct net_device *dev, u32 stringset, u8 * data)
 	switch (stringset) {
 	case ETH_SS_STATS:
 		for (i = 0; i < ARRAY_SIZE(xenvif_stats); i++)
-			memcpy(data + i * ETH_GSTRING_LEN,
-			       xenvif_stats[i].name, ETH_GSTRING_LEN);
+			ethtool_puts(&data, xenvif_stats[i].name);
 		break;
 	}
 }
