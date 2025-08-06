244fb914d4e921bf03691c6f74c7d02f605e3e1e
Rosen Penev <rosenp@gmail.com>
2025-08-06 14:47:46 -0700

    wifi: ath10k: add nvmem support for mac address
    Use of_get_mac_address_nvmem to do so as device_get_mac_address is in
    use for ACPI support.
    
    NVMEM is used in embedded platforms to override default or missing mac
    addresses.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- drivers/net/wireless/ath/ath10k/core.c
+++ drivers/net/wireless/ath/ath10k/core.c
@@ -10,6 +10,7 @@
 #include <linux/module.h>
 #include <linux/firmware.h>
 #include <linux/of.h>
+#include <linux/of_net.h>
 #include <linux/property.h>
 #include <linux/dmi.h>
 #include <linux/ctype.h>
@@ -3451,6 +3452,7 @@ static int ath10k_core_probe_fw(struct ath10k *ar)
 	}
 
 	device_get_mac_address(ar->dev, ar->mac_addr);
+	of_get_mac_address_nvmem(ar->dev->of_node, ar->mac_addr);
 
 	ret = ath10k_core_init_firmware_features(ar);
 	if (ret) {
