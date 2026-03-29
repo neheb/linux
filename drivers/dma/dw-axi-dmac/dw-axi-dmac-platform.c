4e8987891fe280180a8e39630f178e68a6bd941c
Rosen Penev <rosenp@gmail.com>
2026-03-29 14:38:59 -0700

    dmaengine: dw-axi-dmac: simplify allocation
    Use a flexible array member with kzalloc_flex to combine allocations.
    
    Add __counted_by for extra runtime analysis.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- drivers/dma/dw-axi-dmac/dw-axi-dmac-platform.c
+++ drivers/dma/dw-axi-dmac/dw-axi-dmac-platform.c
@@ -297,15 +297,10 @@ static struct axi_dma_desc *axi_desc_alloc(u32 num)
 {
 	struct axi_dma_desc *desc;
 
-	desc = kzalloc_obj(*desc, GFP_NOWAIT);
+	desc = kzalloc_flex(*desc, hw_desc, num, GFP_NOWAIT);
 	if (!desc)
 		return NULL;
 
-	desc->hw_desc = kzalloc_objs(*desc->hw_desc, num, GFP_NOWAIT);
-	if (!desc->hw_desc) {
-		kfree(desc);
-		return NULL;
-	}
 	desc->nr_hw_descs = num;
 
 	return desc;
@@ -342,7 +337,6 @@ static void axi_desc_put(struct axi_dma_desc *desc)
 		dma_pool_free(chan->desc_pool, hw_desc->lli, hw_desc->llp);
 	}
 
-	kfree(desc->hw_desc);
 	kfree(desc);
 	atomic_sub(descs_put, &chan->descs_allocated);
 	dev_vdbg(vchan_chan_dev(&chan->vc), "%s: %d descs put, %d still allocated\n",
