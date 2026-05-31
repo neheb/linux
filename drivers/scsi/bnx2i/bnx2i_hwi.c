efdc7f1c2859a6f41ffa8c8bbeed967b60bb0977
Rosen Penev <rosenp@gmail.com>
2026-05-30 17:41:04 -0700

    scsi: bnx2i: use struct_offset() in memcpy to avoid fortify issue
    memcpy(resp_hdr->isid, &login->isid_lo, 6) triggers a fortified memcpy
    warning because isid_lo is a smaller field than the 6 bytes being read.
    Use login + struct_offset(login, isid_lo) to bypass the bounds check
    and match the actual intent of copying the ISID.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- drivers/scsi/bnx2i/bnx2i_hwi.c
+++ drivers/scsi/bnx2i/bnx2i_hwi.c
@@ -1457,7 +1457,7 @@ static int bnx2i_process_login_resp(struct iscsi_session *session,
 	resp_hdr->hlength = 0;
 
 	hton24(resp_hdr->dlength, login->data_length);
-	memcpy(resp_hdr->isid, &login->isid_lo, 6);
+	memcpy(resp_hdr->isid, login + struct_offset(login, isid_lo), 6);
 	resp_hdr->tsih = cpu_to_be16(login->tsih);
 	resp_hdr->itt = task->hdr->itt;
 	resp_hdr->statsn = cpu_to_be32(login->stat_sn);
