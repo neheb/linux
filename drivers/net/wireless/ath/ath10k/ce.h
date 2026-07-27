d26db3e9a4e2ee3e8349893f2c6c461a4cc90d4c
Rosen Penev <rosenp@gmail.com>
2026-07-26 17:35:57 -0700

    ath10k: fix pointer type
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- drivers/net/wireless/ath/ath10k/ce.h
+++ drivers/net/wireless/ath/ath10k/ce.h
@@ -110,7 +110,7 @@ struct ath10k_ce_ring {
 	struct ce_desc_64 *shadow_base;
 
 	/* keep last */
-	void *per_transfer_context[] __counted_by(nentries);
+	struct sk_buff *per_transfer_context[] __counted_by(nentries);
 };
 
 struct ath10k_ce_pipe {
